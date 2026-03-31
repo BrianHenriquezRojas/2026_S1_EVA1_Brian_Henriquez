0797567ef9b895c18d2d78e9fbf34919f0de3844
BrianHenriquez <brian.henriquez.r@gmail.com>
2026-03-31 11:25:08 -0300

    parte_3

---
+++ src/main/java/cl/duoc/brianhenriquez/model/Prioridad.java
@@ -0,0 +1,5 @@
+package cl.duoc.brianhenriquez.model;
+
+public class Prioridad {
+
+}
--- src/main/java/cl/duoc/brianhenriquez/model/Solicitud.java
+++ src/main/java/cl/duoc/brianhenriquez/model/Solicitud.java
@@ -1,5 +1,29 @@
 package cl.duoc.brianhenriquez.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor  
 public class Solicitud {
 
+    @NotBlank(message = "El nombre del paciente es obligatorio")
+    private String nombrePaciente;
+
+    @NotBlank(message = "La especialidad es obligatoria")
+    private String especialidad;
+
+    @NotNull(message = "El estado es obligatorio")
+    private EstadoSolicitud estado;
+
+    private LocalDateTime fechaRegistro;
+
+    @NotNull(message = "La prioridad es obligatoria")
+    private Prioridad prioridad;
 }
