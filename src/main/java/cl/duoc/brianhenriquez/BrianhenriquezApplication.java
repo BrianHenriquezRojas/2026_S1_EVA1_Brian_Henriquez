b84fc27864a868a4ced86be480d3c2545f43ba68
BrianHenriquez <brian.henriquez.r@gmail.com>
2026-03-31 10:44:24 -0300

    parte_1

---
+++ src/main/java/cl/duoc/brianhenriquez/BrianhenriquezApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.brianhenriquez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrianhenriquezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrianhenriquezApplication.class, args);
+	}
+
+}
